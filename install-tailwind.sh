1370813c03aac1aab292d4317b58a0da36e3bcb5
T.A.V <71072795+tavusion@users.noreply.github.com>
2022-05-06 13:27:52 +0530

    Initial Commit

---
+++ install-tailwind.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Install Tailwind
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🌀
+# @raycast.packageName Dev Setups
+
+# Documentation:
+# @raycast.author T.A.V
+
+npm install -D tailwindcss
+npx tailwindcss init
+
+{ echo 'module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}' 
+} > tailwind.config.js
+
+mkdir src
+cd src || exit
+
+{ echo '<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Description of the Page" />
+        <meta name="theme-color" content="#2c313a" />
+        <link href="/dist/output.css" rel="stylesheet">
+        <title>Title</title>
+    </head>
+    <body></body>
+</html>'
+} > index.html
+
+{ echo '@tailwind base;
+@tailwind components;
+@tailwind utilities;'
+} > input.css
+
+cd ..
+code .
+
